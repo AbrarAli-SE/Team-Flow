5caec90d1f694b93d7115a84fb68cc2451f93ffd
AbrarAli-SE <abrarali.se@gmail.com>
2025-12-05 15:40:26 +0500

    Project Updated

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/ChannelHeader.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/ChannelHeader.tsx
@@ -1,4 +1,5 @@
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import InviteMember from "./member/InviteMember";
 
 interface ChannelHeaderProps {
     channelName?: string | undefined;
@@ -9,7 +10,8 @@ export function ChannelHeader({ channelName }: ChannelHeaderProps) {
     return (
         <div className="flex items-center justify-between h-14 px-4 border-b">
             <h1 className="text-lg font-semibold">#{channelName}</h1>
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-4">
+                <InviteMember />
                 <ThemeToggle />
             </div>
         </div>
---
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/member/InviteMember.tsx
@@ -0,0 +1,75 @@
+
+import { inviteMemberSchema, InviteMembersSchemaType } from "@/app/schemas/member";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserPlus } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function InviteMember() {
+
+    const [open, setOpen] = useState(false);
+
+    const form = useForm({
+        resolver: zodResolver(inviteMemberSchema),
+        defaultValues: {
+            email: '',
+            name: '',
+        }
+    });
+
+    function onSubmit(values: InviteMembersSchemaType) {
+        console.log(values);
+
+    }
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button variant="outline" >
+                    <UserPlus />
+                    Invite Members
+                </Button    >
+            </DialogTrigger>
+            <DialogContent className="sm: max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Invite Members</DialogTitle>
+                    <DialogDescription>
+                        Invite a new member to your workspace by entering their email address below.
+                    </DialogDescription>
+
+                </DialogHeader>
+                <Form {...form}>
+                    <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField control={form.control} name="name" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Enter Name ..." {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+
+                        <FormField control={form.control} name="email" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Enter Email Address ..." {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+
+                        <Button type="submit" >Send Invitation </Button>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog >
+    )
+}   
